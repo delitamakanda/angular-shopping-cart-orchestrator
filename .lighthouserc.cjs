16905036cc11a29136749ba6cfbd9cf63f739903
Délita Makanda <delita.makanda@gmail.com>
2026-07-17 22:00:49 +0200

    Change Lighthouse assertions from 'error' to 'warn' for performance, accessibility, best practices, and SEO categories

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -16,10 +16,10 @@ module.exports = {
         },
         assert: {
             assertions: {
-                'categories:performance': ['error', { minScore: 0.9 }],
-                'categories:accessibility': ['error', { minScore: 0.9 }],
-                'categories:best-practices': ['error', { minScore: 0.9 }],
-                'categories:seo': ['error', { minScore: 0.9 }],
+                'categories:performance': ['warn', { minScore: 0.9 }],
+                'categories:accessibility': ['warn', { minScore: 0.9 }],
+                'categories:best-practices': ['warn', { minScore: 0.9 }],
+                'categories:seo': ['warn', { minScore: 0.9 }],
                 'categories:pwa': ['off'],
                 'large-contentful-paint': ['warn', { maxNumericValue: 2500 }],
                 'cumulative-layout-shift': ['warn', { maxNumericValue: 0.1 }],
